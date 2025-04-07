d7d8d5b517ce484144cbed55f6f31567df2a30f1
Kilian Heyne <kilianheyne@icloud.com>
2025-04-07 17:13:08 +0200

    roughly finished game

--- script.js
+++ script.js
@@ -9,14 +9,14 @@ const htmlQuestions = [
     },
     {
         "question": "Welches HTML-Element wird verwendet, um eine nummerierte Liste zu erstellen?",
-        "answer-1": "<ul>",
-        "answer-2": "<dl>",
-        "answer-3": "<ol>",
-        "answer-4": "<list>",
+        "answer-1": `&lt;ul&gt;`,
+        "answer-2": `&lt;dl&gt;`,
+        "answer-3": `&lt;ol&gt;`,
+        "answer-4": `&lt;list&gt;`,
         "right-answer": 3
     },
     {
-        "question": "Was bewirkt das alt-Attribut im <img>-Tag?",
+        "question": `Was bewirkt das alt-Attribut im &lt;img&gt;-Tag?`,
         "answer-1": "Es verlinkt das Bild auf eine andere Webseite.",
         "answer-2": "Es bestimmt die Bildgröße.",
         "answer-3": "Es beschreibt das Bild für Screenreader und bei Ladeproblemen.",
@@ -25,70 +25,109 @@ const htmlQuestions = [
     },
     {
         "question": "Welches Element ist für den Hauptinhalt einer Webseite vorgesehen?",
-        "answer-1": "<section>",
-        "answer-2": "<main>",
-        "answer-3": "<div>",
-        "answer-4": "<container>",
+        "answer-1": `&lt;section&gt;`,
+        "answer-2": `&lt;main&gt;`,
+        "answer-3": `&lt;div&gt;`,
+        "answer-4": `&lt;container&gt;`,
         "right-answer": 2
     },
     {
         "question": "Wie kann man einen Link so öffnen, dass er in einem neuen Tab angezeigt wird?",
-        "answer-1": '<a href="https://example.com" window="_new">',
-        "answer-2": '<a href="https://example.com" newtab="true">',
-        "answer-3": '<a href="https://example.com" open="new">',
-        "answer-4": '<a href="https://example.com" target="_blank">',
+        "answer-1": `&lt;a href="https://example.com" window="_new"&gt;`,
+        "answer-2": `&lt;a href="https://example.com" newtab="true"&gt;`,
+        "answer-3": `&lt;a href="https://example.com" open="new"&gt;`,
+        "answer-4": `&lt;a href="https://example.com" target="_blank"&gt;`,
         "right-answer": 4
     },
 ]
 
 let currentQuestion = 0; 
+let rightAnsweredQuestions = 0;
 
 function init(){
 
     showQuestions();
 
     const questionsNumRef = document.getElementById('num-all-questions');
-    questionsNumRef.innerHTML = htmlQuestions.length;    
+    questionsNumRef.innerHTML = htmlQuestions.length;
 
-    const currentQuestionNumRef = document.getElementById('current-num');
-    currentQuestionNumRef.innerHTML = htmlQuestions.length - 4;
+    document.getElementById('amount-of-questions').innerHTML = htmlQuestions.length;
 }
 
 function showQuestions(){
     const question = htmlQuestions[currentQuestion];
-    const questionRef = document.getElementById('question-text');
-    questionRef.innerHTML = "";
-    questionRef.innerHTML = question['question'];
+    let percent = currentQuestion / htmlQuestions.length;
+    percent = percent * 100 + "%";
+    document.getElementById('progress-bar').innerHTML = percent;
+    document.getElementById('progress-bar').style.width = percent;
+    
 
-    const answer1Ref = document.getElementById('answer-1');
-    answer1Ref.innerHTML = "";
-    answer1Ref.innerHTML = question['answer-1'];
+    if (currentQuestion >= htmlQuestions.length) {
+        document.getElementById('end-screen').style ='';
+        document.getElementById('question-body').style = 'display: none;'
+        document.getElementById('correctly-answered-questions').innerHTML = rightAnsweredQuestions; 
+    } else {
+        const questionRef = document.getElementById('question-text');
+        questionRef.innerHTML = "";
+        questionRef.innerHTML = question['question'];
 
-    const answer2Ref = document.getElementById('answer-2');
-    answer2Ref.innerHTML = "";
-    answer2Ref.innerHTML = question['answer-2'];
+        const answer1Ref = document.getElementById('answer-1');
+        answer1Ref.innerHTML = "";
+        answer1Ref.innerHTML = question['answer-1'];
 
-    const answer3Ref = document.getElementById('answer-3');
-    answer3Ref.innerHTML = "";
-    answer3Ref.innerHTML = question['answer-3'];
+        const answer2Ref = document.getElementById('answer-2');
+        answer2Ref.innerHTML = "";
+        answer2Ref.innerHTML = question['answer-2'];
 
-    const answer4Ref = document.getElementById('answer-4');
-    answer4Ref.innerHTML = "";
-    answer4Ref.innerHTML = question['answer-4'];    
+        const answer3Ref = document.getElementById('answer-3');
+        answer3Ref.innerHTML = "";
+        answer3Ref.innerHTML = question['answer-3'];
+
+        const answer4Ref = document.getElementById('answer-4');
+        answer4Ref.innerHTML = "";
+        answer4Ref.innerHTML = question['answer-4'];
+
+        document.getElementById('current-num').innerHTML = currentQuestion + 1;
+    }
 }
 
 function answer(clickedAnswer){
     const question = htmlQuestions[currentQuestion];
     let clickedAnswerNum = clickedAnswer.slice(-1);
 
-    let idOfRightAnswer = `answer-3`;
+    let idOfRightAnswer = `answer-${question['right-answer']}`;
 
     if (clickedAnswerNum == question['right-answer']) {
-        console.log('Das ist die richtige Antwort');
         document.getElementById(clickedAnswer).parentNode.classList.add('bg-success');
+        rightAnsweredQuestions++;
         } else {
-            console.log('Das ist die falsche Antwort.');
             document.getElementById(clickedAnswer).parentNode.classList.add('bg-danger');
-            document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success');            
+            document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success');
     }
+
+    const nextButton = document.getElementById('next-button'); 
+    nextButton.disabled = false; 
+}
+
+function nextQuestion(){
+    currentQuestion++;
+    showQuestions();
+    document.getElementById('next-button').disabled = true; 
+    resetAnswers();
+    showQuestions();
+}
+
+function resetAnswers(){
+    document.getElementById('answer-1').parentNode.classList.remove('bg-success', 'bg-danger')
+    document.getElementById('answer-2').parentNode.classList.remove('bg-success', 'bg-danger')
+    document.getElementById('answer-3').parentNode.classList.remove('bg-success', 'bg-danger')
+    document.getElementById('answer-4').parentNode.classList.remove('bg-success', 'bg-danger')
+}
+
+function restartGame(){
+    rightAnsweredQuestions = 0;
+    currentQuestion = 0;
+    document.getElementById('end-screen').style = 'display: none;'
+    document.getElementById('question-body').style = '';
+    init(); 
 }
